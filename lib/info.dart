25614dfde55bd83ee0d1735deb3d206d96328391
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2020-11-20 11:23:24 +0100

    Fixed padding

--- lib/info.dart
+++ lib/info.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 import 'package:image_analyzer/colors.dart';
@@ -11,27 +9,25 @@ class Info extends StatelessWidget {
     return Scaffold(
         backgroundColor: graphite,
         appBar: AppBar(
-
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back, color: Colors.white),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          centerTitle: true,
-        automaticallyImplyLeading: false,
+        centerTitle: true,
         title: Text("Info")),
-        body:Text(
-          "\n\nCreated by: \n"
-              "Diego Seoane González\n"
-          "Guillermo Somoza Morales\n\n\n"
-              "Powered by: https://imagga.com/\n\n\n"
-              "Take a picture with your camera or select it from your gallery"
-              " to scan it.",
-          textAlign: TextAlign.left,
-          textScaleFactor: 1.5,
-          style: TextStyle(
-              color: Colors.white
+        body:Container(
+          padding: EdgeInsets.fromLTRB(30, 10, 30, 10),
+          child: Text(
+            "\n\nCreated by: \n"
+                "Diego Seoane González\n"
+                "Guillermo Somoza Morales\n\n\n"
+                "Powered by: https://imagga.com/\n\n\n"
+                "Take a picture with your camera or select it from your gallery"
+                " to scan it.",
+            textAlign: TextAlign.left,
+            textScaleFactor: 1.5,
+            style: TextStyle(
+                color: Colors.white
+            ),
           ),
-        ),
+        )
+
     );
 
   }
