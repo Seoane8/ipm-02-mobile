77094f7fdb45f5df5ea307680f71155ba1f66169
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2020-11-21 17:15:35 +0100

    Added wait file

---
+++ lib/wait.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:image_analyzer/imageModel.dart';
+import 'package:provider/provider.dart';
+
+class WaitPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ImageModel>(
+      builder: (context, model, child) => Stack(
+        overflow: Overflow.visible,
+        children: [
+          Center(
+            child: Opacity(
+              opacity: 0.3,
+              child: Image.file(model.image)
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 30),
+                FlatButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  )
+                ),
+              ],
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
